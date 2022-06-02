fe1712d56249be9877dd20cae28059729486f0e6
Blessing <blessingnwalozie@gmail.com>
2022-06-02 15:40:35 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * This is a stript that starts with the main finction
- * it uses the puts fun ti frint a string
- * it returns a value zero
+ *  This is a stript that starts with the main finction 
+ *  it uses the puts fun ti frint a string 
+ *  it returns a value zero 
  */
 
 int main(void)

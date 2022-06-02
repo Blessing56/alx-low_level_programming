d67cdf963965cab281b4fd53c05074d6f2a6a0cd
Blessing <blessingnwalozie@gmail.com>
2022-06-02 15:18:50 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * This is a stript that starts with the main finction
+ * it uses the puts fun ti frint a string
+ * it returns a value zero
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle", \n);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * this is a script
+ * it prints a string
+ * using the printf function
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,", \n);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return(0);
 }

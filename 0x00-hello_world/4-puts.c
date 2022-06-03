296da109118656996bbe92a5dd355f4a6072db8f
Blessing <blessingnwalozie@gmail.com>
2022-06-02 17:12:04 -0700

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * This is a stript that starts with the main finction 
- * it uses the puts funtion print a string 
- * it returns a value zero 
+ * * main - program prints with function puts
+ * *
+ * *Return: Always 0
  */
-
-int main(void) /* uses puts() and returns 0  */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

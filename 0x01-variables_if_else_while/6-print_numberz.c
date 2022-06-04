c1029080cc7bbf407527b4f6f41ab218b7104193
Blessing <blessingnwalozie@gmail.com>
2022-06-03 21:31:05 -0700

    IF statements

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,15 +11,12 @@
 int main(void)
 {
 	int digit = 0;
-	
 	while (digit <= 9)
 	{
-		
 	/*convert digit to ASCII representation*/
 		putchar(digit + '0');
 		++digit;
 	}
 	putchar('\n');
-	
 	return (0);
 }

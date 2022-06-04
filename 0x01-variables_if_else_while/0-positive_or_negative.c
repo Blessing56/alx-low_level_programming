6b63a9dac76cb886d1e407d9fa514dc5ce8e4a14
Blessing <blessingnwalozie@gmail.com>
2022-06-03 17:38:57 -0700

    IF, ELSE and WHILE statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 
 
 /**
- * more headers goes there
  * main : positive or negative test
  * Description: prints the status if the value is positive or negative
  * Return: 0 is program is successful

43cf04478900a44082f2c76eda8cb4336d9ac458
Blessing <blessingnwalozie@gmail.com>
2022-06-03 21:12:27 -0700

    IF statements

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-        int digit = 0;
+int digit = 0;
 
-        while (digit <= 9)
-        {
-                printf("%i", digit);
-                ++digit;
-        }
-        printf("\n");
-
-        return (0);
+while (digit <= 9)
+{
+	printf("%i", digit);
+	++digit;
+}
+	printf("\n");
+	return (0);
 }

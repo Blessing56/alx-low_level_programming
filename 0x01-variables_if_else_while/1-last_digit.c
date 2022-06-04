bb9b1cf9008a83838ab69c82b47bc76b44d5a1c5
Blessing <blessingnwalozie@gmail.com>
2022-06-03 18:39:30 -0700

    IF, ELSE and WHILE statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - !-last_digit task
+ * Description: print last digit
+ * Return: 0 if program is successful
+ */
+
+int main(void)
+{
+	int n,digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	digit = n % 10;
+
+	if(digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not o\n", n, digit);
+	return (0);
+}

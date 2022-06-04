fac7a95677b946060a0a0b3fdab95d69c16cc116
Blessing <blessingnwalozie@gmail.com>
2022-06-03 19:50:42 -0700

    IF statements

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,21 +3,19 @@
 /**
  * main - Point of Entry
  *
- * Description: print alphabet in lowercase then in uppercase
+ * Description: print alphabet
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char ch = 'a';
-	char CH = 'A';
-
-	/* prints a - z */
-	while (ch <= 'Z')
+	char alpha = 'a';
+	
+	while (alpha <= 'z')
 	{
-		putchar(CH);
-		++CH;
+		putchar(alpha);
+		alpha++;
 	}
 	putchar('\n');
 

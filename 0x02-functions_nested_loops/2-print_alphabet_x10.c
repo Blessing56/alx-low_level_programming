b6357a46cfaee4dada282fc334c5fc5db6128e45
Blessing <blessingnwalozie@gmail.com>
2022-06-07 15:05:17 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 /**
- * print_alphabet - function that prints lowercase alphabet 10 times
+ * print_alphabet_x10 - function that prints lowercase alphabet 10 times
  */
 
 void print_alphabet_x10(void)

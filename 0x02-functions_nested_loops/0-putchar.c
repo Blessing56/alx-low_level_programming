0ba70b6224b6e9bfe4bd11f6ad4af8ff493a6638
Blessing <blessingnwalozie@gmail.com>
2022-06-07 10:19:04 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-#
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a newline
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	return(0);
+}

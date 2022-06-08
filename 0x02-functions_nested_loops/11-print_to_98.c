abcb51dc1b4cc0bb8d701bb30155237ffe14fc3a
Blessing <blessingnwalozie@gmail.com>
2022-06-07 19:49:55 -0700

    loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
 
 
 /**
- * print_to_98 - function that prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - function that prints all natural numbers from n to 98.
  *
  * @n: integer argument
  *

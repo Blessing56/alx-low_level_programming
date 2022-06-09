7b5e142684f926eb289401d053f3c9c132aac4ee
Blessing <blessingnwalozie@gmail.com>
2022-06-09 09:47:10 -0700

    More finctions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers - prints the munbers from 0 to 9 followed by a new line
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}

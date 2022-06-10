32b55aaa5f76f358a6f6d1c39df8d868d368abb0
Blessing <blessingnwalozie@gmail.com>
2022-06-09 17:21:37 -0700

    More Functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,49 @@
 #include "main.h"
 
 /**
- * main
+ * print_diagonal - prints a line n chars long.
+ * @n: number of \.
+ * Return: void.
  */
 
-void print_diagonal(int n);
+void print_diagonal(int n)
+
+{
+
+	int i, space;
+
+
+
+	if (n <= 0)
+
+	{
+
+		_putchar('\n');
+
+	}
+
+	else
+
+	{
+
+		for (i = 0; i < n; i++)
+
+		{
+
+			for (space = 0; space < i; space++)
+
+			{
+
+				_putchar(' ');
+
+			}
+
+			_putchar(92);
+
+			_putchar('\n');
+
+		}
+
+	}
+
+}

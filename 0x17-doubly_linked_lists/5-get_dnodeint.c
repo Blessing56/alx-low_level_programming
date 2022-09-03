6fc564702f3634ce999db2790bd63ba910fa512e
Blessing56 <blessingnwalozie@gmail.com>
2022-09-03 17:16:29 +0000

    updating the file

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int count = 0;
 	dlistint_t *temp = head;
 
-	while (count < index)
+	while (count < index && temp)
 	{
 		temp = temp->next;
 		count++;
